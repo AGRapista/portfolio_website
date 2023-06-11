8be928b137235386ab0becd87c226dbfc838fbea
ItSeldomRains <agmatthewarapista@gmail.com>
2023-06-11 17:56:25 +0800

    + Used react-icon assets

--- src/components/body/about.js
+++ src/components/body/about.js
@@ -1,85 +1,187 @@
 import React, { useEffect } from "react";
 import "./styles.css";
 
-import logo from "./images/github_logo.png";
-import cv from "./images/cv.png";
-import email_png from "./images/email.png";
-import li from "./images/linkedin.png";
+import { FaPython, FaReact, FaEthereum, FaLinkedin, FaGithub } from "react-icons/fa";
+import { DiJavascript } from "react-icons/di";
+import {
+  SiSolidity,
+  SiDart,
+  SiCplusplus,
+  SiCsharp,
+  SiFlutter,
+  SiFlask,
+} from "react-icons/si";
+import {HiOutlineMail} from "react-icons/hi";
+import {ImProfile} from "react-icons/im";
 
 const AboutPage = () => {
-
   useEffect(() => {
-    const elements = document.getElementsByClassName('fade-in-text');
+    const elements = document.getElementsByClassName("fade-in-text");
     for (let i = 0; i < elements.length; i++) {
-      elements[i].classList.add('fade-in-animation');
+      elements[i].classList.add("fade-in-animation");
     }
   }, []);
 
   return (
     <div className="col-8 col-md-8 about-page">
       <div className="row">
-        <div className="col-12" style={{marginTop: "15%"}}></div>
-        <div className="col-12" style={{ marginBottom: "40px" }}>
-          <h2 className="fade-in-text" style={{ color: "rgb(171, 255, 251)" }}>
+        <div className="col-12" style={{ marginTop: "15%" }}></div>
+        <div className="col-12" id="greetingsContainer">
+          <h3 className="fade-in-text" style={{ color: "rgb(171, 255, 251)" }}>
             Greetings!
-          </h2>
-          <h2 className="fade-in-text fade-in-text-second">
-            I am A.G. Matthew A. Rapista, an aspiring software developer. I create software
-            solutions ranging from web applications, mobile apps, and smart
-            contracts.
-          </h2>
+          </h3>
+          <h3 className="fade-in-text fade-in-text-second">
+            I am A.G. Matthew A. Rapista, an aspiring software developer. I
+            create software solutions ranging from web applications, mobile
+            apps, and smart contracts.
+          </h3>
         </div>
 
-        <div className="col-6">
-          <h3 className="fade-in-text" 
-              style={{ color: "rgb(171, 255, 251)", "animation-fill-mode": "forwards",
-                      "animation-delay": "2s", opacity: 0}}>
-              Programming languages </h3>
-          {["Python", "Javascript", "Solidity", "Dart", "C++", "C#"].map((item, i) => (
-            <div className="skillBubble fade-in-skill" style={{"animation-delay": `${3+(i/3)}s`}}>{item}</div>
+        <div className="col-12 col-md-6">
+          <h3
+            className="fade-in-text"
+            style={{
+              color: "rgb(171, 255, 251)",
+              "animation-fill-mode": "forwards",
+              "animation-delay": "2s",
+              opacity: 0,
+            }}
+          >
+            Programming languages{" "}
+          </h3>
+          {[
+            [<FaPython />, "Python"],
+            [<DiJavascript />, "Javascript"],
+            [<SiSolidity />, "Solidity"],
+            [<SiDart />, "Dart"],
+            [<SiCplusplus />, "C++"],
+            [<SiCsharp />, "C#"],
+          ].map((item, i) => (
+            <div
+              className="skillBubble fade-in-skill"
+              style={{ "animation-delay": `${3 + i / 3}s` }}
+            >
+              {item[0]}
+              {item[1]}
+            </div>
           ))}
-        </div>    
+        </div>
 
-        <div className="col-6">
-          <h3 className="fade-in-text" 
-              style={{ color: "rgb(171, 255, 251)", "animation-fill-mode": "forwards",
-                      "animation-delay": "2s", opacity: 0}}>
-              Technologies </h3>
-          {["Flutter", "ReactJS", "Flask", "Ethereum", "Avalanche"].map((item, i) => (
-            <div className="skillBubble fade-in-skill" style={{"animation-delay": `${3+(i/3)}s`}}>{item}</div>
+        <div className="col-12 col-md-6">
+          <h3
+            className="fade-in-text"
+            style={{
+              color: "rgb(171, 255, 251)",
+              "animation-fill-mode": "forwards",
+              "animation-delay": "2s",
+              opacity: 0,
+            }}
+          >
+            Technologies{" "}
+          </h3>
+          {[
+            [<SiFlutter />, "Flutter"],
+            [<FaReact />, "ReactJS"],
+            [<SiFlask />, "Flask"],
+            [<FaEthereum />, "Ethereum"],
+          ].map((item, i) => (
+            <div
+              className="skillBubble fade-in-skill"
+              style={{ "animation-delay": `${3 + i / 3}s` }}
+            >
+              {item[0]}
+              {item[1]}
+            </div>
           ))}
         </div>
 
-        <div className="col-6">
-          <h3 className="fade-in-text" 
-              style={{ color: "rgb(171, 255, 251)", "animation-fill-mode": "forwards",
-                      "animation-delay": "4s", opacity: 0, marginTop: "40px"}}>
-              Get to know me better </h3>
+        <div className="col-12 col-md-6">
+          <h3
+            className="fade-in-text"
+            style={{
+              color: "rgb(171, 255, 251)",
+              "animation-fill-mode": "forwards",
+              "animation-delay": "4s",
+              opacity: 0,
+              marginTop: "40px",
+            }}
+          >
+            Get to know me better{" "}
+          </h3>
 
-              <a href="https://www.linkedin.com/in/a-g-rapista-584683232/" target="_blank">
-                <button className="fade-in-skill" style={{margin: "5px", borderRadius:"10px", border: "none", backgroundColor: "rgb(40, 193, 248, 0)", animationDelay: "5s"}}>
-                  <img src={li} alt="Button Image" style={{width: "40px", height: "40px"}} />
-                </button>
-              </a>
+          <a
+            href="https://www.linkedin.com/in/a-g-rapista-584683232/"
+            target="_blank"
+          >
+            <button
+              className="fade-in-skill"
+              style={{
+                width: "45px",
+                height: "40px",
+                margin: "5px",
+                borderRadius: "10px",
+                border: "none",
+                backgroundColor: "rgb(40, 193, 248)",
+                animationDelay: "5s",
+              }}
+            >
+              <FaLinkedin/>
+            </button>
+          </a>
 
-              <a href="mailto:amrapista@live.mcl.edu.ph">
-                <button className="fade-in-skill" style={{width: "45px", height: "40px", margin: "5px", borderRadius:"10px", border: "none", backgroundColor: "rgb(122, 247, 94)", animationDelay: "5s"}}>
-                  <img src={email_png} alt="Button Image" style={{width: "30px", height: "30px"}} />
-                </button>
-              </a>
+          <a href="mailto:amrapista@live.mcl.edu.ph">
+            <button
+              className="fade-in-skill"
+              style={{
+                width: "45px",
+                height: "40px",
+                margin: "5px",
+                borderRadius: "10px",
+                border: "none",
+                backgroundColor: "rgb(122, 247, 94)",
+                animationDelay: "5s",
+              }}
+            >
+              <HiOutlineMail/>
+            </button>
+          </a>
 
-              <a href="https://docs.google.com/document/d/1xrkpQuVCz3JoIDmfAOXWdG0YDzDLv2kDRqMs_q6ObQE/edit?usp=sharing" target="_blank">
-                <button className="fade-in-skill" style={{width: "45px", height: "40px", margin: "5px", borderRadius:"10px", border: "none", backgroundColor: "rgb(66, 133, 244)", animationDelay: "5s"}}>
-                    <img src={cv} alt="Button Image" style={{width: "30px", height: "30px"}} />
-                  </button>
-              </a>
-              <a href="https://github.com/AGRapista" target="_blank">
-                <button className="fade-in-skill" style={{ margin: "5px", borderRadius:"10px", border: "none", animationDelay: "5.5s"}}>
-                  <img src={logo} alt="Button Image" style={{width: "40px", height: "40px"}} />
-                </button>
-              </a>
+          <a
+            href="https://docs.google.com/document/d/1xrkpQuVCz3JoIDmfAOXWdG0YDzDLv2kDRqMs_q6ObQE/edit?usp=sharing"
+            target="_blank"
+          >
+            <button
+              className="fade-in-skill"
+              style={{
+                width: "45px",
+                height: "40px",
+                margin: "5px",
+                borderRadius: "10px",
+                border: "none",
+                backgroundColor: "rgb(66, 133, 244)",
+                animationDelay: "5s",
+              }}
+            >
+              <ImProfile/>
+            </button>
+          </a>
+          <a href="https://github.com/AGRapista" target="_blank">
+            <button
+              className="fade-in-skill"
+              style={{
+                width: "45px",
+                height: "40px",
+                margin: "5px",
+                borderRadius: "10px",
+                border: "none",
+                backgroundColor: "rgb(255, 255, 255)",
+                animationDelay: "5s",
+              }}
+            >
+              <FaGithub/>
+            </button>
+          </a>
         </div>
-
       </div>
     </div>
   );
